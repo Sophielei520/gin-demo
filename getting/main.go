3e69934f1a8e4d00a96dbc39592835ccc5aed065
Shuwen He <1201220707@pku.edu.cn>
2019-10-22 11:34:40 +0800

    getting

---
+++ getting/main.go
@@ -0,0 +1,21 @@
+// 浏览器或控制台执行如下命令
+// http://localhost:8080/ping
+
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/user", getting)
+	router.Run()
+}
+
+func getting(c *gin.Context) {
+	c.String(http.StatusOK, "zhangsan")
+}
